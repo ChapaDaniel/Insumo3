24c51c97644c3a9d1176e2f5ee566c0c2a47a46e
Chapa Daniel <Chapadaniel479@gmail.com>
2023-01-09 00:26:27 -0500

    mjr rot

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { Componente1Component } from './componente1/componente1.component'; 
+import { Componente1Component } from './componente1/componente1.component';
 import { Componente2Component } from './componente2/componente2.component';
 import { Componente3Component } from './componente3/componente3.component';
 import { Componente4Component } from './componente4/componente4.component';
